282ad0f7f7bbc0fde49e5742bc01c8b160ec66e9
Andy Kirk <andy@andykirk.net>
2021-06-14 21:48:50 +0100

    Update DirProcessCopyHandlerTwig.php

--- src/DirProcessCopyHandlerTwig.php
+++ src/DirProcessCopyHandlerTwig.php
@@ -1,7 +1,7 @@
 <?php
 namespace DirProcessCopyHandlerTwig;
 
-require_once dirname(__DIR__) . '/vendor/autoload.php';
+#require_once dirname(__DIR__) . '/vendor/autoload.php';
 use \Michelf\Markdown;
 
 /**
